c626a01dcaf435d9305c1d15ceeddacca18fa03f
Matthew Jung <sexysangmin@users.noreply.github.com>
2025-08-01 10:46:38 +0900

    로고 이미지 문제 수정: WebP 지원 추가 및 PNG fallback 복원

--- nav.js
+++ nav.js
@@ -17,7 +17,10 @@ function loadNavigation() {
                     <!-- 로고 -->
                     <div class="flex items-center">
                         <a href="${pathPrefix}index.html" class="flex items-center">
-                            <img src="${pathPrefix}images/logo.png" alt="자유와혁신 로고" class="h-16 w-auto">
+                            <picture>
+                                <source srcset="${pathPrefix}images/logo.webp" type="image/webp">
+                                <img src="${pathPrefix}images/logo.png" alt="자유와혁신 로고" class="h-16 w-auto">
+                            </picture>
                         </a>
                     </div>
                     
